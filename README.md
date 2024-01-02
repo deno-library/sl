e29d929fd50e10f553cbd0d2695e3175935cc77e
zhangfuxing <zhangfuxing@cmict.chinamobile.com>
2024-01-02 17:01:40 +0800

    add code

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # sl
-Deno porting of sl (https://github.com/mtoyoda/sl) command.
+
+Deno porting of sl (https://github.com/mtoyoda/sl) command. You can use any options (-a / -F / -l / -c) that the original software uses.
+
+## run
+
+```ts
+deno run sl.ts
+```
---
+++ cursor.ts
@@ -0,0 +1,137 @@
+// const isTerminalApp = Deno.env.get("TERM_PROGRAM") === "Apple_Terminal";
+
+const ESC = "\x1B[";
+
+// const SAVE = isTerminalApp ? "\x1B7" : ESC + "s";
+// const RESTORE = isTerminalApp ? "\x1B8" : ESC + "u";
+const SAVE = "\x1B7";
+const RESTORE = "\x1B8";
+const POSITION = "6n";
+const HIDE = "?25l";
+const SHOW = "?25h";
+const SCROLL_UP = "T";
+const SCROLL_DOWN = "S";
+
+const UP = "A";
+const DOWN = "B";
+const RIGHT = "C";
+const LEFT = "D";
+
+const CLEAR_RIGHT = "0K";
+const CLEAR_LEFT = "1K";
+const CLEAR_LINE = "2K";
+
+const CLEAR_DOWN = "0J";
+const CLEAR_UP = "1J";
+const CLEAR_SCREEN = "2J";
+
+const NEXT_LINE = "1E";
+const PREV_LINE = "1F";
+const HOME = "H";
+
+class Cursor {
+  private encoder = new TextEncoder();
+  private writer = Deno.stdout.writable.getWriter();
+
+  private stdoutWrite(msg: string) {
+    this.writer.write(this.encoder.encode(msg));
+  }
+
+  private cursor(action: string) {
+    this.stdoutWrite(ESC + action);
+  }
+
+  write(msg: string) {
+    this.stdoutWrite(msg);
+  }
+
+  breakLine() {
+    this.stdoutWrite("\n");
+  }
+
+  hide() {
+    this.cursor(HIDE);
+  }
+
+  show() {
+    this.cursor(SHOW);
+  }
+
+  clearUp() {
+    this.cursor(CLEAR_UP);
+  }
+
+  clearDown() {
+    this.cursor(CLEAR_DOWN);
+  }
+
+  clearScreen() {
+    this.cursor(CLEAR_SCREEN);
+  }
+
+  to(x: number, y: number) {
+    this.cursor(`${y};${x}${HOME}`);
+  }
+
+  goUp(y = 1) {
+    this.cursor(y + UP);
+  }
+
+  goDown(y = 1) {
+    this.cursor(y + DOWN);
+  }
+
+  goLeft(x = 1) {
+    this.cursor(x + LEFT);
+  }
+
+  goRight(x = 1) {
+    this.cursor(x + RIGHT);
+  }
+
+  save() {
+    this.cursor(SAVE);
+  }
+
+  restore() {
+    this.cursor(RESTORE);
+  }
+
+  position() {
+    this.cursor(POSITION);
+  }
+
+  scrollUp() {
+    this.cursor(SCROLL_UP);
+  }
+
+  scrollDown() {
+    this.cursor(SCROLL_DOWN);
+  }
+
+  clearLeft() {
+    this.cursor(CLEAR_LEFT);
+  }
+
+  clearRight() {
+    this.cursor(CLEAR_RIGHT);
+  }
+
+  clearLine() {
+    this.cursor(CLEAR_LINE);
+  }
+
+  nextLine() {
+    this.cursor(NEXT_LINE);
+  }
+
+  prevLine() {
+    this.cursor(PREV_LINE);
+  }
+
+  goHome() {
+    this.cursor(HOME);
+  }
+}
+
+export const cursor = new Cursor();
